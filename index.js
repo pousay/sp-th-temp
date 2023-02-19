c0908b4af722c771222cc584a0676efc0c656087
pousay <110107147+pousay@users.noreply.github.com>
2023-02-19 21:37:44 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var x = document.querySelector(".contact-items");
+var b = document.querySelector(".contact-toggler");
+
+b.addEventListener("click", () => {
+  if (x.classList.contains("d-none")) {
+    x.classList.remove("d-none");
+    b.innerHTML = `<svg  width="35" height="35" fill="var(--d-blue)" viewBox="0 0 16 16">
+        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+      </svg>`;
+  } else {
+    x.classList.add("d-none");
+    b.innerHTML = `<svg width="35" height="35" fill="var(--d-blue)" viewBox="0 0 16 16">
+    <path
+      fill-rule="evenodd"
+      d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
+    />
+  </svg>`;
+  }
+});
